f63d4837b2baae07354f53f6bc7063989ff043e3
gamsteron <gamsteron@gmail.com>
2018-10-19 12:14:53 +0200

    0.03

--- Champions/GamsteronBlitzcrank.lua
+++ Champions/GamsteronBlitzcrank.lua
@@ -1,3 +1,6 @@
+local GamsteronBlitzVer = 0.03
+local LocalCore, Menu, Orbwalker, TargetSelector, ObjectManager, Damage, Spells
+
 do
     if _G.GamsteronBlitzcrankLoaded or myHero.charName ~= "Blitzcrank" then
         return
@@ -12,9 +15,10 @@ do
     if _G.GamsteronCoreUpdated then
         return
     end
+    LocalCore = _G.GamsteronCore
 
-    local success, version = AutoUpdate({
-        version = 0.02,
+    local success, version = LocalCore:AutoUpdate({
+        version = GamsteronBlitzVer,
         scriptPath = SCRIPT_PATH .. "GamsteronBlitzcrank.lua",
         scriptUrl = "https://raw.githubusercontent.com/gamsteron/GOS-External/master/Champions/GamsteronBlitzcrank.lua",
         versionPath = SCRIPT_PATH .. "GamsteronBlitzcrank.version",
@@ -42,9 +46,9 @@ local Q_AUTO_HITCHANCE = 3
 
 local QData =
 {
-    Type = SPELLTYPE_LINE, Aoe = false, From = myHero,
+    Type = LocalCore.SPELLTYPE_LINE, Aoe = false, From = myHero,
     Delay = 0.25, Radius = 70, Range = 925, Speed = 1800,
-    Collision = true, MaxCollision = 0, CollisionObjects = { COLLISION_MINION, COLLISION_YASUOWALL, COLLISION_ENEMYHERO }
+    Collision = true, MaxCollision = 0, CollisionObjects = { LocalCore.COLLISION_MINION, LocalCore.COLLISION_YASUOWALL, LocalCore.COLLISION_ENEMYHERO }
 }
 
 local function LoadMenu()
@@ -53,13 +57,14 @@ local function LoadMenu()
         Menu:MenuElement({name = "Q Auto", id = "auto", type = _G.MENU })
             Menu.auto:MenuElement({id = "enabled", name = "Enabled", value = true, callback = function(value) Q_AUTO_ON = value end})
             Menu.auto:MenuElement({name = "Use on:", id = "useon", type = _G.MENU })
-                OnEnemyHeroLoad(function(hero) Menu.auto.useon:MenuElement({id = hero.charName, name = hero.charName, value = true}) end)
+                LocalCore:OnEnemyHeroLoad(function(hero) Menu.auto.useon:MenuElement({id = hero.charName, name = hero.charName, value = true}) end)
             Menu.auto:MenuElement({id = "hitchance", name = "Hitchance", value = 3, drop = { "Collision", "Normal", "High", "Immobile" }, callback = function(value) Q_AUTO_HITCHANCE = value end })
+        Menu:MenuElement({name = "Version " .. tostring(GamsteronBlitzVer), type = _G.SPACE, id = "verblitzspace"})
 end
 
 local function QLogic()
     if Q_AUTO_ON and Spells:IsReady(_Q, { q = 0.3, w = 0.3, e = 0.3, r = 0.3 } ) then
-        local EnemyHeroes = ObjectManager:GetEnemyHeroes(QData.Range, false, HEROES_SPELL)
+        local EnemyHeroes = ObjectManager:GetEnemyHeroes(QData.Range, false, LocalCore.HEROES_SPELL)
         local qList = {}
         for i = 1, #EnemyHeroes do
             local hero = EnemyHeroes[i]
@@ -68,9 +73,9 @@ local function QLogic()
                 table.insert(qList, hero)
             end
         end
-        local qTarget = TargetSelector:GetTarget(qList, DAMAGE_TYPE_MAGICAL)
+        local qTarget = TargetSelector:GetTarget(qList, LocalCore.DAMAGE_TYPE_MAGICAL)
         if qTarget then
-            if CastSpell(HK_Q, qTarget, myHero, QData, Q_AUTO_HITCHANCE) then
+            if LocalCore:CastSpell(HK_Q, qTarget, myHero, QData, Q_AUTO_HITCHANCE) then
                 return
             end
         end
@@ -82,10 +87,10 @@ AddLoadCallback(function()
 
     LoadMenu()
 
-    local Interrupter = __Interrupter()
+    local Interrupter = LocalCore:__Interrupter()
     Interrupter:OnInterrupt(function(enemy, activeSpell)
         if Q_INTERRUPTER_ON and Spells:IsReady(_Q, { q = 0.3, w = 0.3, e = 0.3, r = 0.3 } ) then
-            CastSpell(HK_Q, enemy, myHero, QData, 4)
+            LocalCore:CastSpell(HK_Q, enemy, myHero, QData, 4)
         end
     end)
 
