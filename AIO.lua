ed30a40ff9124c3597ec434d778157db2029555b
txx1255301153 <47805666+txx1255301153@users.noreply.github.com>
2019-03-09 16:27:08 +0800

    Update AIO.lua

--- AIO.lua
+++ AIO.lua
@@ -1,10 +1,4 @@
- local Files = {
-        Lua = {
-            Path = SCRIPT_PATH,
-            Name = "Xerath.lua",
-            Url = "raw.githubusercontent.com/qqwer1/GoS-Lua/master/External/LazyXerath.lua"
-        }
-         local function DownloadFile(url, path, fileName)
-            DownloadFileAsync(url, path .. fileName, function() end)
-            while not FileExist(path .. fileName) do end
-        end
+if myHero.charName ~= "Ezreal"
+ then
+ require('GamsteronAIO')
+end
