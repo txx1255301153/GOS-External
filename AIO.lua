c4c7f769424c033b93504aaa3aa7fd2fd6f5fddb
txx1255301153 <47805666+txx1255301153@users.noreply.github.com>
2019-03-09 17:23:58 +0800

    Update AIO.lua

--- AIO.lua
+++ AIO.lua
@@ -1,6 +1,43 @@
 if myHero.charName~='Ezreal' 
  then
+ local name = "GoS-U External.lua" 
+
+local func = loadfile(name)--加载文件 
+end
+if myHero.charName~='KogMaw' 
+ then
  local name = "GamsteronAIO.lua" 
+
+local func = loadfile(name)--加载文件 
+end
+
+if myHero.charName~='Vayne' 
+ then
+ local name = "GamsteronAIO.lua" 
+
+local func = loadfile(name)--加载文件 
+end
+if myHero.charName~='Brand' 
+ then
+ local name = "GamsteronAIO.lua" 
+
+local func = loadfile(name)--加载文件 
+end
+if myHero.charName~='Morgana' 
+ then
+ local name = "GamsteronAIO.lua" 
+
+local func = loadfile(name)--加载文件 
+end
+if myHero.charName~='Xerath' 
+ then
+ local name = "LazyXerath.lua" 
+
+local func = loadfile(name)--加载文件 
+end
+if myHero.charName~='Jinx' 
+ then
+ local name = "GoS-U External.lua" 
 
 local func = loadfile(name)--加载文件 
 end
